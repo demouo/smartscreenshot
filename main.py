daa32df6431a1d1b367f3bec7aaff3389bac6f6b
demouo <121025366+demouo@users.noreply.github.com>
2023-09-19 15:32:13 +0800

    优化用户体验加入进度条、一键清空输入和错误提示的功能

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import threading
+from tkinter import ttk
 
 import win32com.client
 import tkinter as tk
@@ -35,7 +36,7 @@ def read_file():
 def read_content(content):
     '''
         split the content to two arr with &
-        '''
+    '''
     big_split = '|'
     small_split = '@'
     content = content.split(big_split)
@@ -98,26 +99,36 @@ def add_text_in_para(doc, curr, max, comment):
 
 def exec_you_get(u, p):
     command = "you-get -O " + p + " --format=dash-flv480 " + u
-    print("command=" + command)
     os.system(command)
-    # os.system("you-get " + url + " -O " + path)
 
 
-def process_sss(video_name, url_path, content):
-    print("name=" + video_name)
-    print("url=" + url_path)
+def process_sss(video_name, url_path, content, progress_bar, _l_error_msg, base):
+    progress = progress_bar["value"]
     base_dir_path = "D:/onedrive/桌面/word_bilibili/"
     # video_name, url_path, content = read_file()
     video_path = base_dir_path + video_name
-    print("path=" + video_path)
-    # you-get
-    exec_you_get(url_path, video_path)
-    video_path += ".mp4"
-    comment, _times = read_content(content)
+    try:
+        # you-get
+        exec_you_get(url_path, video_path)
+        progress += 20
+        progress_bar["value"] = progress
+        progress_bar.update()
+        video_path += ".mp4"
+        comment, _times = read_content(content)
+    except:
+        progress_bar["value"] = 0
+        progress_bar.update()
+        _l_error_msg.grid(row=base + 4, column=2)
+        return
     # app
     app = win32com.client.Dispatch("Word.Application")
     # app.visible=1
     doc = init_docx(app, video_name)
+
+    progress += 10
+    progress_bar["value"] = progress
+    progress_bar.update()
+
     # add first comment
     add_text_in_para(doc, -1, len(comment), comment)
     # do screenshot
@@ -138,6 +149,10 @@ def process_sss(video_name, url_path, content):
         # 在当前的段落中插入图片
         para_range.InlineShapes.AddPicture(img_file)
         os.remove(img_file)
+        if progress <= 89:
+            progress += 10
+            progress_bar["value"] = progress
+            progress_bar.update()
     # 保存文档
     doc.SaveAs(base_dir_path + video_name + ".docx")
     # 关闭Word应用程序
@@ -145,38 +160,66 @@ def process_sss(video_name, url_path, content):
     # 删除视频
     os.remove(video_path)
 
+    progress_bar["value"] = 100
+    progress_bar.update()
 
-def init_gui():
-    window = tk.Tk()
+
+def _clear_text_(*args):
+    for _ in args:
+        _.delete(0,tk.END)
+
+
+def _init_gui():
+    _window = tk.Tk()
     base = 1
-    l1 = tk.Label(window, text="欢迎来到SmartScreenshot")
-    l1.grid(row=base - 1, column=0)
+    _l_welcome = tk.Label(_window, text="欢迎来到SmartScreenshot")
+    _l_welcome.grid(row=base - 1, column=0)
 
     name = tk.StringVar()
     url = tk.StringVar()
     content = tk.StringVar()
 
-    for i in range(3):
-        if i == 0:
-            text = "请输入名称"
-            e = tk.Entry(window, textvariable=name)
-        elif i == 1:
-            text = "请输入b站url"
-            e = tk.Entry(window, textvariable=url)
-        else:
-            text = "请输入内容"
-            e = tk.Entry(window, textvariable=content)
-        l = tk.Label(window, text=text)
-        l.grid(row=base + i, column=0)
-        e.grid(row=base + i, column=1)
+    text = "请输入名称"
+    _l_name = tk.Label(_window, text=text)
+    _e_name = tk.Entry(_window, textvariable=name)
+    _l_name.grid(row=base,column=0)
+    _e_name.grid(row=base,column=1)
 
-    button = tk.Button(window, text="提交",
-                       command=lambda: threading.Thread(target=process_sss, args=(name.get(), url.get(), content.get()),
-                                                        daemon=True).start())
-    button.grid(row=base + 3, column=1)
+    text = "请输入网址"
+    _l_url = tk.Label(_window, text=text)
+    _e_url = tk.Entry(_window, textvariable=url)
+    _l_url.grid(row=base+1, column=0)
+    _e_url.grid(row=base+1, column=1)
 
-    window.mainloop()
+    text = "请输入内容"
+    _l_content = tk.Label(_window, text=text)
+    _e_content = tk.Entry(_window, textvariable=content)
+    _l_content.grid(row=base+2, column=0)
+    _e_content.grid(row=base+2, column=1)
 
+    # 创建一个进度条并放在底部
+    _progress_bar = ttk.Progressbar(_window, orient="horizontal", length=150, mode="determinate")
+    _progress_bar.grid(row=base + 3, column=1)
+
+    def start_process():
+        _progress_bar["value"] = 10
+        _progress_bar.update()
+        # 创建一个守护进程来执行任务，并传递进度条
+        threading.Thread(target=process_sss,
+                         args=(name.get(), url.get(), content.get(), _progress_bar, _l_error_msg, base),
+                         daemon=True).start()
+
+    # err msg
+    _l_error_msg = tk.Label(_window, text="提交出错，请重试！")
+    _l_error_msg.grid_remove()
+
+    _b_commit = tk.Button(_window, text="提交", command=start_process)
+    _b_commit.grid(row=base + 4, column=1)
+
+    _b_clear = tk.Button(_window, text="清空", command=lambda: _clear_text_(_e_name, _e_url, _e_content))
+    _b_clear.grid(row=base + 4, column=0)
+
+    _window.mainloop()
 
 if __name__ == '__main__':
-    init_gui()
+    _init_gui()
